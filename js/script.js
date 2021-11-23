b17b27c4181b6e9590410ff453bc9ebe83b98c74
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2021-11-23 17:31:41 +0100

    add-color

--- js/script.js
+++ js/script.js
@@ -10,12 +10,26 @@
 
 
 
-
+const container = document.querySelector(".container");
 for(let i = 1; i < 101; i++){
+    const div = document.createElement('div');
+    div.classList.add('box_div');
+    container.append(div);
+    div.innerHTML = i; 
     
-    if (i % 3 == 0); {
-        document.getElementById("box").innerHTML("frizz");
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        div.innerHTML ="frizzbuzz";
+        div.style.backgroundColor ="pink"
     }
+    else if(i % 3 == 0){
+      div.innerHTML = "frizz";
+      div.style.backgroundColor ="red";
+    } else if (i % 5 == 0){
+        div.innerHTML ="buzz";
+        div.style.backgroundColor ="yellow"
+    } 
+    
         
         
 }
