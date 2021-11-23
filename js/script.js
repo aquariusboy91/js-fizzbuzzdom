6a64a8c6bd62c4d5a904917993750e57e8d550a6
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2021-11-23 16:06:35 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+// let numero = parseInt(prompt("inserisci"));
+
+// let mult_3 = numero % 3;
+// if (numero % 3 == 0) {
+//     console.log ("è mult di 3");
+// } else {
+//     console.log ("non è mult di 3");
+// }
+
+
+
+
+for(let i = 1; i < 101; i++){
+    
+    if (i % 3 == 0); {
+        document.getElementById("box").innerHTML("frizz");
+    }
+        
+        
+}
